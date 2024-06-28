6a07b31a0ca290f89a1f9b08320b281aa639b399
Yugam Verma <yugamverma003@gmail.com>
2024-06-28 22:03:08 +0530

    Nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import FeaturedSection from "@/components/FeaturedSection";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased
+    bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedSection />
+      <WhyChooseUs />
+      <TestimonialCards />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
+    </main>
+  );
+}
